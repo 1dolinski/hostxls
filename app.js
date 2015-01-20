8f21bbb6c64891eb77e4c7f22260adceb092a5bd
ebbflowgo <christopher.dolinski@gmail.com>
2015-01-20 00:02:20 -0500

    make it better

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+
+var redis = require("redis");
+	client = redis.createClient();
+
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
