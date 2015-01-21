d9d222fc1e3510b6515362a99e9c7fe659b67638
ebbflowgo <christopher.dolinski@gmail.com>
2015-01-21 09:14:56 -0500

    basic cr

--- app.js
+++ app.js
@@ -2,50 +2,40 @@ var express = require('express');
 var app = express();
 
 var redis = require("redis");
-	client = redis.createClient();
-
-
-
-
-
+client = redis.createClient();
 
 // redis basic works
-client.on("error", function (err) {
-	console.log("Error " + err);
+client.on("error", function(err) {
+    console.log("Error " + err);
 });
-
-
 
 app.use(express.static(__dirname + '/public'));
 
 // load basic template
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/views/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/views/index.html');
 })
 
-
 // test set redis
-app.get('/set/:hashkey/:key/:value', function (req, res) {
-	client.hset(req.params["hashkey"], req.params["key"], req.params["value"], redis.print);
-	res.send("Submitted:");
+app.get('/set/:hashkey/:key/:value', function(req, res) {
+    client.hset(req.params["hashkey"], req.params["key"], req.params["value"], redis.print);
+    res.send("Submitted:");
 })
 
 // test get 
-app.get('/get/:hashkey', function (req, res) {
-	client.hgetall(req.params["hashkey"], function( err, obj ) {
-		console.log(obj);
-	  res.send(obj);
-	})
+app.get('/get/:hashkey', function(req, res) {
+    client.hgetall(req.params["hashkey"], function(err, obj) {
+        console.log(obj);
+        res.send(obj);
+    })
 })
 
-
-
 // run server
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port)
 
 })
